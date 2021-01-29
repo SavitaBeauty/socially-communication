4b17f1ac09b6cb49c7db4c874acf3f186df2f173
SavitaBeauty <savitabns25@gmail.com>
2021-01-29 13:02:53 +0530

    React Router Setup

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
+{/* 
 ReactDOM.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>, 
+  </React.StrictMode>,  
   document.getElementById('root')
 );
 
+*/}
+
+ReactDOM.render(<App />, document.getElementById('root'));
