fec9f3af8a9a005baa1b7cca36229a3e5a42fecc
SavitaBeauty <savitabns25@gmail.com>
2021-03-03 16:19:08 +0530

    Starting On The Profile

--- client/src/components/profiles/ProfilesItem.js
+++ client/src/components/profiles/ProfilesItem.js
@@ -1,7 +1,6 @@
-import React, { Fragment, useEffect } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
 
 const ProfileItem = ({
     profile: {
